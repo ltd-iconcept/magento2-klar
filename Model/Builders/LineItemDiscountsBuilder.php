c47386ad8a50dd3a5641cf4332508a79c2c1f797
Frank Birzle <mail@frankbirzle.de>
2024-01-25 15:34:06 +0100

    save

--- Model/Builders/LineItemDiscountsBuilder.php
+++ Model/Builders/LineItemDiscountsBuilder.php
@@ -1,7 +1,17 @@
 <?php
+
+/**
+ * @copyright Copyright (c) 2023 Magebit (https://magebit.com/)
+ * @author    <nils@magebit.com>
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
-namespace ICT\Klar\Model\Builders;
+namespace Magebit\Klar\Model\Builders;
 
 use ICT\Klar\Api\Data\DiscountInterface;
 use ICT\Klar\Api\Data\DiscountInterfaceFactory;
